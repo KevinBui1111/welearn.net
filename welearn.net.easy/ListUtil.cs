44b6e679744ab576b20bf2093ec6c55a8ad54648
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2023-09-02 14:03:02 +0700

    init draft

---
+++ welearn.net.easy/ListUtil.cs
@@ -0,0 +1,33 @@
+﻿namespace welearn.net.easy;
+
+public class ListUtil {
+    static readonly Random rng = new();
+        
+    public static List<(long from, long to)> Distribute(long from, long to, int groupCount) {
+        long size = to - from,
+            groupSize = size / groupCount,
+            remain = size % groupCount,
+            sizeAllRemain = from + remain * (groupSize + 1);
+        ;
+        var listGroup = new List<(long from, long to)>();
+        while (from < sizeAllRemain) {
+            listGroup.Add((from, from += groupSize + 1));
+        }
+        while (from < to) {
+            listGroup.Add((from, from += groupSize));
+        }
+
+        return listGroup;
+    }
+        
+    public static void Shuffle<T> (T[] array)
+    {
+        var n = array.Length;
+        while (n > 1) 
+        {
+            var k = rng.Next(n--);
+            (array[n], array[k]) = (array[k], array[n]);
+        }
+    }
+
+}
