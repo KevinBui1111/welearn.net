a1dd5c9f94aea6dd058e52d8e6c0afd5b57890da
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2023-09-14 14:05:31 +0700

    protected internal, private protected

--- Learn/welearn.net.learn.O0PKevin/Accessibility.cs
+++ Learn/welearn.net.learn.O0PKevin/Accessibility.cs
@@ -6,8 +6,23 @@ public class Accessibility {
     private TopLevelPublicClass _topLevelPublicClass;
     //TopLevelInternalClass b; // can't access
     // TopLevelDefaultInternalClass b // can't access
-    public void TopLevelPublicClass() {
+    public void Access_TopLevelPublicClass() {
         _topLevelPublicClass = new ();
-        // a.Age // can't access
+        // _topLevelPublicClass.Age // can't access
+    }
+}
+
+internal class InheritFromTopLevelPub : TopLevelPublicClass {
+    public void AccessParent() {
+        /* cannot access
+        PrivateProtectedMethod();
+        var p = PrivateProtectedField;
+        */
+        // can access protected & protected internal
+        ProtectedInternalMethod();
+        ProtectedInternalField = 11;
+        
+        ProtectedMethod();
+        ProtectedField = 11;
     }
 }
--- Learn/welearn.net.learn.OOP/AccessModifier.cs
+++ Learn/welearn.net.learn.OOP/AccessModifier.cs
@@ -9,11 +9,67 @@ class TopLevelDefaultInternalClass { }
 internal class TopLevelInternalClass {
     //Why use a public method in an internal class?
     public string Name = "aaa";
+
+    public void Access_TopLevelPublicClass() {
+        var topLevelPub = new TopLevelPublicClass();
+        /* can access type or member has `internal` modifier,
+        cannot access
+        topLevelPub.ProtectedField;
+        topLevelPub.ProtectedMethod
+        */
+        topLevelPub.InternalAge = 11;
+        topLevelPub.ProtectedInternalMethod();
+        topLevelPub.ProtectedInternalField = 11;
+    }
 }
 
 public class TopLevelPublicClass {
     public string Name { get; set; }
     public int Number { get; set; }
 
-    internal int Age { get; set; }
+    internal int InternalAge { get; set; }
+
+    // implicit private modifier
+    /*private*/void PrivateMethod() { }
+
+    protected int ProtectedField = 19;
+    protected void ProtectedMethod() {
+        //can be accessed by types derived from the class,
+        //don't care same / different assembly.
+    }
+
+    private protected int PrivateProtectedField = 19;
+    private protected void PrivateProtectedMethod() {
+        //can be accessed by types derived from the class, and
+        //that are declared within its containing assembly.
+    }
+
+    protected internal int ProtectedInternalField = 19;
+    protected internal void ProtectedInternalMethod() {
+        //can be accessed by types derived from the class, or
+        //class are declared within its containing assembly.
+
+        //cannot access from non-derived class from different assembly.
+    }
+}
+
+internal class InheritFromTopLevelPub : TopLevelPublicClass {
+    public void AccessParent() {
+        // can access all except private.
+
+        PrivateProtectedMethod();
+        PrivateProtectedField = 11;
+
+        ProtectedInternalMethod();
+        ProtectedInternalField = 11;
+
+        ProtectedMethod();
+        ProtectedField = 11;
+
+        Console.WriteLine($@"
+            {PrivateProtectedField}
+            {ProtectedInternalField}
+            {ProtectedField}"
+        );
+    }
 }
