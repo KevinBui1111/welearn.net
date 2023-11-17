ecb92bd6f7c40a982ea4f42085fd44be253f8148
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2023-11-17 23:37:57 +0700

    MaxPoints2

--- Algo/welearn.net.algo.piece/HackerRank/MaxPointsALine.cs
+++ Algo/welearn.net.algo.piece/HackerRank/MaxPointsALine.cs
@@ -43,6 +43,32 @@ public static class MaxPointsALine {
         int GetIndex(int n, int m, int len) =>
             len * n - n * (n + 1) / 2 + 1 + m - n - 1 - 1;
     }
+    
+    public static int MaxPoints2(int[][] points) {
+        var max = 0;
+        for (var i = 0; i < points.Length; ++i) {
+            var dicAngle = new Dictionary<float, int>();
+
+            for (var j = i + 1; j < points.Length; ++j) {
+                var angle = CalcAngle(points[i], points[j]);
+
+                if (dicAngle.TryGetValue(angle, out var c))
+                    dicAngle[angle] = c + 1;
+                else
+                    dicAngle[angle] = 1;
+
+                if (max < dicAngle[angle]) max = dicAngle[angle];
+            }
+        }
+        
+        return max + 1;
+
+        float CalcAngle(IReadOnlyList<int> point1, IReadOnlyList<int> point2) {
+            float disY = point1[1] - point2[1];
+            if (disY == 0) return float.PositiveInfinity;
+            return (point1[0] - point2[0]) / disY;
+        }
+    }
 
     public static void main() {
         var dicAngle = new Dictionary<float, int>();
