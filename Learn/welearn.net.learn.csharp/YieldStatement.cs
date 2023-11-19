3274fcb443625d17e271a38e94115a49a12ccf2f
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2023-11-19 12:07:36 +0700

    SkipLastN

--- Learn/welearn.net.learn.csharp/YieldStatement.cs
+++ Learn/welearn.net.learn.csharp/YieldStatement.cs
@@ -1,30 +1,39 @@
 namespace welearn.net.learn.csharp;
 
-public class YieldStatement {
+public static class YieldStatement {
     public static void Test() {
         var first5 = FirstFivePositiveNumber();
         Console.WriteLine("caller: FirstFivePositiveNumber");
         foreach (var i in first5) {
             Console.Write($"{i} ");
         }
-        
+
         Console.WriteLine("caller 2: FirstFivePositiveNumber");
         foreach (var i in first5) {
             Console.Write($"{i} ");
         }
-        
+
         Console.WriteLine();
 
         Console.WriteLine("ProduceEvenNumbers");
         foreach (var i in ProduceEvenNumbers(9)) {
             Console.Write($"{i} ");
         }
-        
+
         Console.WriteLine();
 
         Console.WriteLine("TakeWhilePositive");
         Console.WriteLine(string.Join(" ", TakeWhilePositive(new[] { 2, 3, 4, 5, -1, 3, 4 })));
         // Output: 2 3 4 5
+
+        var Seq = Enumerable.Range(1, 10);
+
+        Console.WriteLine(
+            string.Join(", ", Seq.TakeAllButLast())
+        );
+        Console.WriteLine(
+            string.Join(", ", Seq.SkipLastN(3))
+        );
     }
 
     private static IEnumerable<int> FirstFivePositiveNumber() {
@@ -44,7 +53,7 @@ public class YieldStatement {
         for (var i = 0; i <= upto; i += 2) {
             yield return i;
         }
-        
+
         yield return upto * 10;
     }
 
@@ -55,5 +64,36 @@ public class YieldStatement {
             else
                 yield break;
         }
+    }
+
+    //https://stackoverflow.com/a/1779135/836376
+    private static IEnumerable<T> TakeAllButLast<T>(this IEnumerable<T> source) {
+        var it = source.GetEnumerator();
+        bool hasRemainingItems = false;
+        bool isFirst = true;
+        T item = default(T);
+
+        do {
+            hasRemainingItems = it.MoveNext();
+            if (hasRemainingItems) {
+                if (!isFirst) yield return item;
+                item = it.Current;
+                isFirst = false;
+            }
+        } while (hasRemainingItems);
+    }
+    //https://stackoverflow.com/a/1779135/836376
+    private static IEnumerable<T> SkipLastN<T>(this IEnumerable<T> source, int n) {
+        var it = source.GetEnumerator();
+        bool hasRemainingItems = false;
+        var cache = new Queue<T>(n + 1);
+
+        do {
+            if (hasRemainingItems = it.MoveNext()) {
+                cache.Enqueue(it.Current);
+                if (cache.Count > n)
+                    yield return cache.Dequeue();
+            }
+        } while (hasRemainingItems);
     }
 }
