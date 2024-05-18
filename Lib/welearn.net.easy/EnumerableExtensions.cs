0056ac1330522a063eb0e7b0269b347acf6e7ed2
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2024-05-18 19:34:29 +0700

    ForEach

--- Lib/welearn.net.easy/EnumerableExtensions.cs
+++ Lib/welearn.net.easy/EnumerableExtensions.cs
@@ -5,11 +5,32 @@ public static class EnumerableExtensions {
         Console.WriteLine($"{{{string.Join(", ", enumerable)}}}");
     }
 
-    public static IEnumerable<int> IndexOfWhere<T>(this IEnumerable<T> enumerable, Predicate<T> predicate, bool oneBaseIndex = true) {
+    public static IEnumerable<int> IndexOfWhere<T>(
+        this IEnumerable<T> enumerable, Predicate<T> predicate, bool oneBaseIndex = true
+    ) {
         var oneIndex = oneBaseIndex ? 1 : 0;
         return enumerable
             .Select((o, idx) => new { o, idx = idx + oneIndex })
             .Where(o => predicate(o.o))
             .Select(o => o.idx);
+    }
+
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action) {
+        foreach (var item in source) {
+            action(item);
+            yield return item;
+        }
+    }
+
+    public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T, int> action) {
+        var index = -1;
+        foreach (var item in source) {
+            checked {
+                ++index;
+            }
+
+            action(item, index);
+            yield return item;
+        }
     }
 }
