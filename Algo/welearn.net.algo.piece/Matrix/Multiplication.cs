9dc7a217eb249146ea9d2eace99c42b5a6341d2e
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2024-06-19 17:43:10 +0700

    basic Multiply matrix

---
+++ Algo/welearn.net.algo.piece/Matrix/Multiplication.cs
@@ -0,0 +1,23 @@
+namespace welearn.net.algo.piece.Matrix;
+
+public class Multiplication {
+    public static int[,] Multiply(int[,] matrixA, int[,] matrixB) {
+        var resRow = matrixA.GetLength(0);
+        var resCol = matrixB.GetLength(1);
+        var resMatrix = new int[resRow, resCol];
+
+        for (var r = 0; r < resRow; ++r)
+            for (var c = 0; c < resCol; ++c)
+                resMatrix[r, c] = CalcRc(r, c);
+
+        return resMatrix;
+        
+        int CalcRc(int r, int c) {
+            var res = 0;
+            for (var i = 0; i < resCol; i++)
+                res += matrixA[r, i] * matrixB[i, c];
+
+            return res;
+        }
+    }
+}
