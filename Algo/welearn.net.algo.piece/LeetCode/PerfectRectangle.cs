c09f8fdaa4bbbb3a3dcee0013210ac4e8836d1ca
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2024-06-22 15:32:02 +0700

    IsRectangleCover2

--- Algo/welearn.net.algo.piece/LeetCode/PerfectRectangle.cs
+++ Algo/welearn.net.algo.piece/LeetCode/PerfectRectangle.cs
@@ -1,5 +1,6 @@
 namespace welearn.net.algo.piece.LeetCode;
 
+//https://leetcode.com/problems/perfect-rectangle/solutions/
 public class PerfectRectangle {
     public bool IsRectangleCover(int[][] rectangles) {
         (int left, int bottom, int right, int top) boundary = (int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
@@ -33,5 +34,30 @@ public class PerfectRectangle {
             (rect1, rect2) = (rect2, rect1);
 
         return rect2[1] < rect1[3];
+    }
+    
+    // refer solution from leetcode, O(n)
+    public bool IsRectangleCover2(int[][] rectangles) {
+        var mapCorners = new HashSet<(int, int)>();
+        (int left, int bottom, int right, int top) boundary = (int.MaxValue, int.MaxValue, int.MinValue, int.MinValue);
+        foreach (var r in rectangles) {
+            if (r[0] < boundary.left) boundary.left = r[0];
+            if (r[1] < boundary.bottom) boundary.bottom = r[1];
+            if (boundary.right < r[2]) boundary.right = r[2];
+            if (boundary.top < r[3]) boundary.top = r[3];
+
+            if (!mapCorners.Add((r[0], r[1]))) mapCorners.Remove((r[0], r[1]));
+            if (!mapCorners.Add((r[0], r[3]))) mapCorners.Remove((r[0], r[3]));
+            if (!mapCorners.Add((r[2], r[1]))) mapCorners.Remove((r[2], r[1]));
+            if (!mapCorners.Add((r[2], r[3]))) mapCorners.Remove((r[2], r[3]));
+        }
+        
+        return mapCorners.Count == 4;
+    }
+
+
+    public static void Test() {
+        int[][] rects = [[1, 1, 3, 3], [3, 1, 4, 2], [3, 2, 4, 4], [1, 3, 2, 4], [2, 3, 3, 4]];
+        new PerfectRectangle().IsRectangleCover2(rects);
     }
 }
