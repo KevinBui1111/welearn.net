0d3647b579e9c9b20f01e8bf2bea2b630039203b
Khanh.BuiD <Khanh.BuiD@homecredit.vn>
2024-06-22 17:24:13 +0700

    chore: example

--- Algo/welearn.net.algo.ExactCover/SetupTeam.cs
+++ Algo/welearn.net.algo.ExactCover/SetupTeam.cs
@@ -2,11 +2,37 @@
 
 namespace welearn.net.algo.ExactCover;
 
-/// <summary>
-/// Knuth's Algorithm X, exact cover problem
-/// </summary>
+/*
+Knuth's Algorithm X, exact cover problem
+Lập ra team gồm candidates có đủ kỹ năng cho trước
+
+vd:
+kỹ năng cần có của team {
+- biết python
+- biết java
+- giỏi giao tiếp (SOCIAL)
+- biết automation test (AT)
+- biết quản lý (MAN)
+- biết phân tích data (DA)
+- biết kiến trúc hệ thống (SA)
+}
+
+Candidate 1: { SOCIAL, MAN, DA }
+Candidate 2: { python, AT, SA }
+Candidate 3: { AT, MAN, SA }
+Candidate 4: { java, SA }
+Candidate 5: { python, AT }
+Candidate 6: { java, SOCIAL, DA, SA }
+
+Với input trên, thì Team cần lập gồm các candidate: 1, 4, 5
+--
+Team cần thỏa điều kiện:
+- phải cover đủ kỹ năng đề ra
+- team member không trùng kỹ năng với nhau
+Không tìm được thì báo trả mảng rỗng
+*/
 public class SetupTeam {
-    public static void Find(int n, int[][] candidates) {
+    public static List<int[]> FindTeam(int n, int[][] candidates) {
         var skillsMark = new bool[n + 1];
         var candidatesMark = new bool[candidates.Length];
         var totalRemainingSkills = n;
@@ -14,26 +40,21 @@ public class SetupTeam {
 
         Find(0);
 
-        successTeams.ForEach(t => t.PrintConsole());
-        
-        return;
+        return successTeams;
 
         void Find(int from) {
             for (var i = from; i < candidates.Length; ++i) {
-                if (canFit(candidates[i])) {
-                    // join team
-                    JoinTeam(i, join: true);
+                if (!CanFit(candidates[i])) continue;
+
+                // join team
+                JoinTeam(i, join: true);
                     
-                    if (StillNeedSkill())
-                        // find next
-                        Find(i + 1);
-                    else {
-                        // find out
-                        successTeams.Add(ToTeam());
-                    }
-                    // leave team
-                    JoinTeam(i, join: false);
-                }
+                if (StillNeedSkill()) // find next
+                    Find(i + 1);
+                else // find out
+                    successTeams.Add(ToTeam());
+                // leave team
+                JoinTeam(i, join: false);
             }
         }
 
@@ -57,9 +78,9 @@ public class SetupTeam {
             }
         }
 
-        bool canFit(int[] subSet) {
-            for (var i = 0; i < subSet.Length; ++i) {
-                if (skillsMark[subSet[i]]) return false;
+        bool CanFit(int[] candidate) {
+            for (var i = 0; i < candidate.Length; ++i) {
+                if (skillsMark[candidate[i]]) return false;
             }
 
             return true;
@@ -67,7 +88,7 @@ public class SetupTeam {
     }
     
     public static void Test() {
-        Find(7, [
+        var successTeams = FindTeam(7, [
             [1, 4, 7],
             [1, 4],
             [4, 5, 7],
@@ -75,6 +96,22 @@ public class SetupTeam {
             [2, 3, 6, 7],
             [2, 7]
         ]);
+        successTeams.ForEach(t => t.PrintConsole());
+        Console.WriteLine("done");
+        
+        successTeams = FindTeam(9, [
+            [3, 6, 7, 9],
+            [9],
+            [1, 4, 7],
+            [4, 5, 8],
+            [1, 2],
+            [3, 5, 6],
+            [3, 4, 9],
+            [5, 6],
+            [2, 8],
+            [7, 8],
+        ]);
+        successTeams.ForEach(t => t.PrintConsole());
         Console.WriteLine("done");
     }
 }
